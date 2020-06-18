c24ecd11f16eabd386934bd0b408a65cdec2a79e
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 03:36:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence1)
   new_sentence = []
   
   original_sentence.each do | letters |
-    new_sentence.join(letters.reverse)
+    new_sentence << letters.reverse
   end
   
   return new_sentence
